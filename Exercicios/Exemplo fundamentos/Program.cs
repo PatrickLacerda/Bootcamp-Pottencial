3ab713d252a4ba0f17b008dc95bfaed328d045c1
Patrick Candido Lacerda <patrick.lacerda@jurong.com.br>
2022-09-27 17:34:16 -0300

    Aula 27-09-2022

--- Exercicios/Exemplo fundamentos/Program.cs
+++ Exercicios/Exemplo fundamentos/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exemplo_fundamentos.models ;
+
+Pessoa pessoa1  = new Pessoa();
+pessoa1.Nome ="Patrick Candido Lacerda";
+pessoa1.Idade = 36;
+pessoa1.Apresentar();
