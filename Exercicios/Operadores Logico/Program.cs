029888d2c9c87fd6482f6f08e21d05845d2fa050
PatrickLacerda <patricklacerda@gmail.com>
2022-10-05 00:15:42 -0300

    Conclusão da aula de operadores logicos

--- Exercicios/Operadores Logico/Program.cs
+++ Exercicios/Operadores Logico/Program.cs
@@ -1,30 +1,125 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 
-//bool ehMaiorDeIdade = false;
 
-//bool possuiAutorizacaoDoResponsavel = true;
+bool choveu = true;
+bool estaTarde = false;
 
-int quantidadeEmEstoque = 3;
-int quantidadeCompra =0;
-bool  possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
-
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade compra {quantidadeCompra}");
-Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
-
-if (quantidadeCompra == 0)
+if (!choveu && !estaTarde)
 {
-    Console.WriteLine("Venda invalida");
-}
-
-else if (possivelVenda)
-{
-    Console.WriteLine("Venda realizada.");
+    Console.WriteLine("vou pedalar");
 }
 else
 {
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque");
+    Console.WriteLine("vou pedalar um outro dia");
 }
+// bool possuiPresencaMinima = false;
+// double media = 7.5;
+// if (possuiPresencaMinima && media>=7)
+// {
+//     Console.WriteLine("Aprovado");
+// } 
+// else 
+// {
+//     Console.WriteLine("Reprovado!");
+// }
+
+// Console.WriteLine("Digite uma letra");
+// string letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("vogal");
+//         break;
+    
+
+//     default:
+//         Console.WriteLine("Não é uma vogal");
+//         break;
+// }
+
+
+
+
+
+
+
+
+// // if (letra == "a" ||
+// //     letra == "e" ||
+// //     letra == "i" ||
+// //     letra == "o" ||
+// //     letra == "u")
+
+// // {
+// //     Console.WriteLine("Vogal");
+// // }
+// // else
+// // {
+// //     Console.WriteLine("não é uma vogal");
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //bool ehMaiorDeIdade = false;
+
+// //bool possuiAutorizacaoDoResponsavel = true;
+
+// // int quantidadeEmEstoque = 3;
+// // int quantidadeCompra = 0;
+// // bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// // Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// // Console.WriteLine($"Quantidade compra {quantidadeCompra}");
+// // Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+// // if (quantidadeCompra == 0)
+// // {
+// //     Console.WriteLine("Venda invalida");
+// // }
+
+// // else if (possivelVenda)
+// // {
+// //     Console.WriteLine("Venda realizada.");
+// // }
+// // else
+// // {
+// //     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque");
+// // }
+
+
+
+
+
 
 
 
