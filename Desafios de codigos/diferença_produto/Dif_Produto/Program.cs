76b0846f52615dddb08d37225ae6bfa70153ae81
Patrick Candido Lacerda <patrick.lacerda@jurong.com.br>
2022-10-10 13:46:23 -0300

    Desafio estacionamento

--- Desafios de codigos/diferença_produto/Dif_Produto/Program.cs
+++ Desafios de codigos/diferença_produto/Dif_Produto/Program.cs
@@ -20,6 +20,26 @@ Exemplo 1:
 
 */
 
+using System;
+ 
+class Solution {
+    public static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+ 
+        for (int i = 1; i <= n; i++) {
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+            if (n % i ==  0) {
+                count++;
+            }
+            if (count > 3) {
+                Console.WriteLine(false);
+            }
+        }
+         Console.WriteLine(count == 3);
+    }
+}
+
 class Solution
 {
     public static void Main(string[] args)
@@ -40,7 +60,7 @@ class Solution
             {
                 count++;
             }
-            if (count >= 3)
+            if (count > 3)
             {
                 Console.WriteLine(true);
             }
