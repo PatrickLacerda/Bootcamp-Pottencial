01f48ca2f900a1c003c1baefa1a277173d0391c0
Patrick Candido Lacerda <patrick.lacerda@jurong.com.br>
2022-10-12 16:09:12 -0300

    Aula Pratica 12/10/2022

---
+++ Exercicios/Intermediario/Propriedade_Métodos_e_Construtores_C#/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Propriedade_Métodos_e_Construtores_C_.Models
+{
+    public class Pessoa
+    {
+        public string  Nome { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
