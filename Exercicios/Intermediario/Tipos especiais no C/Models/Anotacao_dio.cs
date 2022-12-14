44085b75850ff5bca6b30ff8d1ae0e6bd4cadcec
Patrick Candido Lacerda <patrick.lacerda@jurong.com.br>
2022-12-14 16:16:04 -0300

    Itens

---
+++ Exercicios/Intermediario/Tipos especiais no C/Models/Anotacao_dio.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tipos_especiais_no_C.Models
+{
+    public class Anotacao_dio
+    {
+        
+    }
+}
--- Exercicios/Intermediario/Tipos especiais no C/Program.cs
+++ Exercicios/Intermediario/Tipos especiais no C/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Tipos_especiais_no_C.Models;
-Console.WriteLine("Hello, World!");
+using System.Globalization;
+
+
+//quando coloca ? logo apos o tipo da variavel, está informando o sistema para aceitar um valor adicionar que é do tipo nulo
+
+bool? desejaReceberEmail = true;
+
+//hasvalue vai verificar se é diferente de nulo
+// desejaReceberEmail.Value ele verifica o proprio valor se é true
+
+if (desejaReceberEmail.HasValue && desejaReceberEmail.Value)
+{
+    Console.WriteLine("O usuario optou por receber e-mail.");
+}
+else
+{   
+    Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail");
+}
