40b2db20fc60cc84c31679e80d65fada732572bb
Patrick Candido Lacerda <patrick.lacerda@jurong.com.br>
2022-12-21 17:26:58 -0300

    Orientada a objetos

---
+++ Exercicios/Programação orientada a objeto/Program.cs
@@ -0,0 +1,7 @@
+﻿using Programação_orientada_a_objeto.models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Patrick";
+p1.Idade = 37;
+
+p1.Apresentar();
---
+++ Exercicios/Programação orientada a objeto/models/Teoria.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programação_orientada_a_objeto.models
+{
+    public class Teoria
+    {
+        //CLASSE CRIADA PARA COLOCAR TEORIAS DO CONTEUDO APRENDIZADO
+
+        // PROGRAMAÇÃO ORIENTADA A OBJETO  É UM PARADIGMA DE PROGRAMAÇÃO, OU SEJA, CORRESPONDE A UMA TÉCNICA DE PROGRAMAÇÃO PARA UM FIM ESPECIFICO.
+        // DENTRO DAS TÉCNICAS, EXISTEM 4 PILARES
+        // ABSTRAÇÃO
+        // ENCAPSULAMENTO
+        // HERANÇA
+        // POLIMORFISMO 
+
+    }
+}
